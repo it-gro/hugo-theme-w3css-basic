5bc09773c04ade92fccc6b1599aa29147f1d4da6
it-gro <13005925+it-gro@users.noreply.github.com>
2023-03-06 23:10:51 +0100

    Cookie consent - first dismiss, then use externals

--- static/js/custom.top.js
+++ static/js/custom.top.js
@@ -1,2 +1,7 @@
 /* -------------------------------------------------- */
 /* custom included at to top */
+function getCookie (e) {
+  var t = "; " + document.cookie,
+    i = t.split("; " + e + "=");
+  return 2 != i.length ? void 0 : i.pop().split(";").shift();
+}
